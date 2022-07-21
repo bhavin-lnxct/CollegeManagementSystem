6fd83eed9b9f3d9d255947033d30d29db5d5879d
bhavin-02 <jeetkariya77777@gmail.com>
2022-07-21 10:27:34 +0530

    Lable Added commit

--- CollegeManagementSystem/Home.Designer.cs
+++ CollegeManagementSystem/Home.Designer.cs
@@ -36,11 +36,14 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
-            this.ovalShape5 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
-            this.ovalShape4 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
-            this.ovalShape3 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
             this.ovalShape2 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.label6 = new System.Windows.Forms.Label();
             this.ovalShape1 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape3 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape4 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape5 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.label7 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -70,6 +73,8 @@
             // 
             this.panel2.BackgroundImage = global::CollegeManagementSystem.Properties.Resources.kimberly_farmer_lUaaKCUANVI_unsplash;
             this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
             this.panel2.Controls.Add(this.label5);
             this.panel2.Controls.Add(this.label4);
             this.panel2.Controls.Add(this.label3);
@@ -88,11 +93,11 @@
             this.label5.BackColor = System.Drawing.Color.RoyalBlue;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(500, 253);
+            this.label5.Location = new System.Drawing.Point(515, 253);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(87, 24);
+            this.label5.Size = new System.Drawing.Size(52, 24);
             this.label5.TabIndex = 3;
-            this.label5.Text = "Students";
+            this.label5.Text = "Fees";
             // 
             // label4
             // 
@@ -100,11 +105,11 @@
             this.label4.BackColor = System.Drawing.Color.RoyalBlue;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(495, 79);
+            this.label4.Location = new System.Drawing.Point(510, 79);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 24);
+            this.label4.Size = new System.Drawing.Size(59, 24);
             this.label4.TabIndex = 2;
-            this.label4.Text = "Students";
+            this.label4.Text = "Users";
             // 
             // label3
             // 
@@ -112,11 +117,11 @@
             this.label3.BackColor = System.Drawing.Color.RoyalBlue;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(304, 162);
+            this.label3.Location = new System.Drawing.Point(303, 161);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 24);
+            this.label3.Size = new System.Drawing.Size(89, 24);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Students";
+            this.label3.Text = "Teachers";
             // 
             // label2
             // 
@@ -136,44 +141,15 @@
             this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
             this.shapeContainer1.Name = "shapeContainer1";
             this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape1,
             this.ovalShape5,
             this.ovalShape4,
             this.ovalShape3,
-            this.ovalShape2,
-            this.ovalShape1});
+            this.ovalShape1,
+            this.ovalShape2});
             this.shapeContainer1.Size = new System.Drawing.Size(684, 381);
             this.shapeContainer1.TabIndex = 0;
             this.shapeContainer1.TabStop = false;
-            // 
-            // ovalShape5
-            // 
-            this.ovalShape5.BackColor = System.Drawing.Color.RoyalBlue;
-            this.ovalShape5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ovalShape5.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
-            this.ovalShape5.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
-            this.ovalShape5.Location = new System.Drawing.Point(298, 124);
-            this.ovalShape5.Name = "ovalShape5";
-            this.ovalShape5.Size = new System.Drawing.Size(101, 100);
-            // 
-            // ovalShape4
-            // 
-            this.ovalShape4.BackColor = System.Drawing.Color.RoyalBlue;
-            this.ovalShape4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ovalShape4.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
-            this.ovalShape4.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
-            this.ovalShape4.Location = new System.Drawing.Point(488, 42);
-            this.ovalShape4.Name = "ovalShape4";
-            this.ovalShape4.Size = new System.Drawing.Size(101, 100);
-            // 
-            // ovalShape3
-            // 
-            this.ovalShape3.BackColor = System.Drawing.Color.RoyalBlue;
-            this.ovalShape3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ovalShape3.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
-            this.ovalShape3.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
-            this.ovalShape3.Location = new System.Drawing.Point(493, 214);
-            this.ovalShape3.Name = "ovalShape3";
-            this.ovalShape3.Size = new System.Drawing.Size(101, 100);
             // 
             // ovalShape2
             // 
@@ -181,9 +157,21 @@
             this.ovalShape2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ovalShape2.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
             this.ovalShape2.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
-            this.ovalShape2.Location = new System.Drawing.Point(97, 215);
+            this.ovalShape2.Location = new System.Drawing.Point(95, 215);
             this.ovalShape2.Name = "ovalShape2";
-            this.ovalShape2.Size = new System.Drawing.Size(101, 100);
+            this.ovalShape2.Size = new System.Drawing.Size(122, 122);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(97, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 24);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Department";
             // 
             // ovalShape1
             // 
@@ -191,9 +179,61 @@
             this.ovalShape1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ovalShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
             this.ovalShape1.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
-            this.ovalShape1.Location = new System.Drawing.Point(88, 40);
+            this.ovalShape1.Location = new System.Drawing.Point(287, 112);
             this.ovalShape1.Name = "ovalShape1";
-            this.ovalShape1.Size = new System.Drawing.Size(101, 100);
+            this.ovalShape1.Size = new System.Drawing.Size(122, 122);
+            // 
+            // ovalShape3
+            // 
+            this.ovalShape3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ovalShape3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ovalShape3.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovalShape3.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            this.ovalShape3.Location = new System.Drawing.Point(76, 29);
+            this.ovalShape3.Name = "ovalShape3";
+            this.ovalShape3.Size = new System.Drawing.Size(122, 122);
+            // 
+            // ovalShape4
+            // 
+            this.ovalShape4.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ovalShape4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ovalShape4.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovalShape4.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            this.ovalShape4.Location = new System.Drawing.Point(476, 29);
+            this.ovalShape4.Name = "ovalShape4";
+            this.ovalShape4.Size = new System.Drawing.Size(122, 122);
+            // 
+            // ovalShape5
+            // 
+            this.ovalShape5.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ovalShape5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ovalShape5.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovalShape5.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            this.ovalShape5.Location = new System.Drawing.Point(479, 203);
+            this.ovalShape5.Name = "ovalShape5";
+            this.ovalShape5.Size = new System.Drawing.Size(122, 122);
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.rectangleShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape1.BorderColor = System.Drawing.Color.Transparent;
+            this.rectangleShape1.FillColor = System.Drawing.Color.RoyalBlue;
+            this.rectangleShape1.Location = new System.Drawing.Point(205, 346);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(89, 23);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(221, 348);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 19);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Logout";
             // 
             // Home_Form
             // 
@@ -221,15 +261,18 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
         private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
-        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
         private System.Windows.Forms.Label label2;
-        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape5;
-        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape4;
-        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape3;
         private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape5;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape4;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape3;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private System.Windows.Forms.Label label7;
 
     }
 }
